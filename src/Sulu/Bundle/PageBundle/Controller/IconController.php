bf7aad500501040c1a198e9a8e24f77d1b9637ff
Adrian Sieber <adrian.sieber@massiveart.com>
2022-11-04 11:28:15 +0100

    added iconcontroller

---
+++ src/Sulu/Bundle/PageBundle/Controller/IconController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\PageBundle\Controller;
+
+use FOS\RestBundle\View\ViewHandlerInterface;
+use HandcraftedInTheAlps\RestRoutingBundle\Routing\ClassResourceInterface;
+use Sulu\Component\Rest\AbstractRestController;
+use Sulu\Component\Rest\ListBuilder\CollectionRepresentation;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IconController extends AbstractRestController implements ClassResourceInterface
+{
+    public function __construct(
+        ViewHandlerInterface $viewHandler,
+    ) {
+        parent::__construct($viewHandler);
+    }
+
+    /**
+     * Returns teaser by ids (get-parameter).
+     *
+     * @return Response
+     */
+    public function cgetAction(Request $request)
+    {
+        return $this->handleView(
+            $this->view(
+                new CollectionRepresentation(
+                    'data test icon',
+                    'icon'
+                )
+            )
+        );
+    }
+}
